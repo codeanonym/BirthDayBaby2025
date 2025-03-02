b8312818406fce6bd12ef4340d5731f6e4def11b
DESKTOP-5JALLN3\LENOVO <balaji.sahil45987@gmail.com>
2025-03-03 04:18:17 +0530

    landing page

--- introindex.js
+++ introindex.js
@@ -1,12 +1,6 @@
-// const hero_data = document.querySelector('.hero-data');
-// hero_data.style.setProperty('--animate-duration', '1s');
-
-// const element = document.querySelector('.home-socials');
-// element.style.setProperty('--animate-duration', '2s');
-
 
 var typed = new Typed('#typed-js', {
-    strings: ['Full Stack', 'Dapps / Web3', 'Data Engineering', 'MLOps', 'Freelance'],
+    strings: ['My clumsy princess', 'The exquisite goddess', 'My best friend', 'My passionate lover', 'For me | Everything'],
     startDelay: 0,
     typeSpeed: 50,
     backSpeed: 50,
