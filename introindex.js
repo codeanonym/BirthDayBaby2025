4ec6ec22af8210d0cfbf85c57cf80b20f3588727
DESKTOP-5JALLN3\LENOVO <balaji.sahil45987@gmail.com>
2025-03-03 03:58:07 +0530

    added intro

---
+++ introindex.js
@@ -0,0 +1,18 @@
+// const hero_data = document.querySelector('.hero-data');
+// hero_data.style.setProperty('--animate-duration', '1s');
+
+// const element = document.querySelector('.home-socials');
+// element.style.setProperty('--animate-duration', '2s');
+
+
+var typed = new Typed('#typed-js', {
+    strings: ['Full Stack', 'Dapps / Web3', 'Data Engineering', 'MLOps', 'Freelance'],
+    startDelay: 0,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+  });
+  
